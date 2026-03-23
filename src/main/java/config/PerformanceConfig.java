c413af01ebd60d6b745ea8610f45616b8e295baf
Clessio <clessio.silva1989@gmail.com>
2026-03-23 16:08:21 -0300

    first steps

---
+++ src/main/java/config/PerformanceConfig.java
@@ -0,0 +1,14 @@
+package config;
+
+public class PerformanceConfig {
+	
+	//CARGA
+	public int usuarios = 50;   //threads
+	public int repeticoes = 10;		//request por users
+	public int rampUpSegundos = 5;			//time para subir todos users ramp-up
+	public int duracaoSegundos = 30;
+	
+	//SLA
+	
+	
+}
