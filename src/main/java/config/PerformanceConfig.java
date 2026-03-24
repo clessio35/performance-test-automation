ac1ac62ac4ba6bd2f091f3c3b34635e5930c42ed
Clessio <clessio.silva1989@gmail.com>
2026-03-24 17:01:55 -0300

    UPDATE - performanceConfig,

--- src/main/java/config/PerformanceConfig.java
+++ src/main/java/config/PerformanceConfig.java
@@ -3,15 +3,15 @@ package config;
 public class PerformanceConfig {
 	
 	//CARGA
-	public int usuarios = 50;   //threads
+	public int usuarios = 10;   //threads
 	public int repeticoes = 10;		//request por users
 	public int rampUpSegundos = 5;			//time para subir todos users ramp-up
 	public int duracaoSegundos = 30;
 	
 	//SLA
 	public int tempoMedioMax = 500; 		//tempo medio maximo em ms
-	public int p95Max = 800;		//percentil 95 maximo  em ms
+	public int p95Max = 1000;		//percentil 95 maximo  em ms
 	public int erroMax = 1; 		//taxa maxima de erro em %
-	public int throughputMin = 20; 		// Thoughput minimo request por segundo
+	public int throughputMin = 1; 		// Thoughput minimo request por segundo
 	
 }
